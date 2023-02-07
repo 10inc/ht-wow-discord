75955bdff20ebd7518754f531078bcc64a032fd6
Ethan Sonza <ethanjericho@yahoo.com>
2023-02-07 17:55:07 +1000

    Add port

--- helpers/conn.js
+++ helpers/conn.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2')
 const connection = mysql.createConnection({
   host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: 'acore_auth'
-})
+  database: "acore_auth",
+});
 
 module.exports = {
   connection: connection
