956c789cfda9004a827e481ab4f3cdbd05331bf3
Ethan Sonza <ethanjericho@yahoo.com>
2023-02-18 04:10:26 +1000

    Add files via upload

---
+++ HPT_spawnregions.lua
@@ -0,0 +1,12 @@
+function SpawnRegions()
+	return {
+		{ name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+		{ name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
+		{ name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
+		{ name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
+		{ name = "AZSpawn", file = "media/maps/AZSpawn/spawnpoints.lua" },
+		{ name = "Blackwood", file = "media/maps/Blackwood/spawnpoints.lua" },
+		{ name = "Facility-7", file = "media/maps/Facility-7/spawnpoints.lua" },
+		{ name = "RavenCreek", file = "media/maps/RavenCreek/spawnpoints.lua" },
+	}
+end
